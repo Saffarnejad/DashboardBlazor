3a2a0a460da8111d5e91ad805a6b8c04ba86edd3
Amin Saffarnejad <vira1368@gmail.com>
2024-11-09 23:10:32 +0330

    User key type changed to int.

--- DashboardBlazor/Data/User.cs
+++ DashboardBlazor/Data/User.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 namespace DashboardBlazor.Data
 {
     // Add profile data for application users by adding properties to the User class
-    public class User : IdentityUser
+    public class User : IdentityUser<int>
     {
     }
 
