25d1cd8bcddba4cf76d8b06cedadccf6993c8809
Amin Saffarnejad <vira1368@gmail.com>
2024-11-09 22:08:54 +0330

    The name of the ApplicationUser class was changed to User.

---
+++ DashboardBlazor/Data/User.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DashboardBlazor.Data
+{
+    // Add profile data for application users by adding properties to the User class
+    public class User : IdentityUser
+    {
+    }
+
+}
