57a2a7ba5ef550ac2c2095f863e0a19ad5d8188e
Amin Saffarnejad <vira1368@gmail.com>
2024-11-11 22:27:52 +0330

    Set base class for IdentityDbContext.

--- DashboardBlazor/Data/ApplicationDbContext.cs
+++ DashboardBlazor/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace DashboardBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User, Role, int>(options)
+        IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>(options)
     {
     }
 }
