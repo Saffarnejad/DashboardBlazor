a70faa7b1244f5c39eb0042a18ceecd634430f53
Amin Saffarnejad <vira1368@gmail.com>
2024-11-13 02:11:58 +0330

    Added RoleClaimConfiguration.

---
+++ DashboardBlazor/Data/Configurations/RoleClaimConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class RoleClaimConfiguration : IEntityTypeConfiguration<RoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<RoleClaim> builder)
+        {
+            builder.ToTable("RoleClaims");
+        }
+    }
+}
