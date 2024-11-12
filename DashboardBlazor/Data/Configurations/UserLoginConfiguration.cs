9cc89dd8d6d82d9accde520e9ae81a71da446bf9
Amin Saffarnejad <vira1368@gmail.com>
2024-11-13 02:24:25 +0330

    Added UserLoginConfiguration.

---
+++ DashboardBlazor/Data/Configurations/UserLoginConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class UserLoginConfiguration : IEntityTypeConfiguration<UserLogin>
+    {
+        public void Configure(EntityTypeBuilder<UserLogin> builder)
+        {
+            builder.ToTable("UserLogins");
+        }
+    }
+}
