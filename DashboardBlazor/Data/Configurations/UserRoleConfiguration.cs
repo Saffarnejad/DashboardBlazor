143bc831f14fa50dd56c8bced0d8c5cd847635a0
Amin Saffarnejad <vira1368@gmail.com>
2024-11-12 22:03:35 +0330

    Added UserRoleConfiguration.

---
+++ DashboardBlazor/Data/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder
+                .ToTable("UserRoles")
+                .HasData(new UserRole { UserId = 1, RoleId = 1 });
+        }
+    }
+}
