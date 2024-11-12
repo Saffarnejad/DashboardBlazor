5a1e61ecc987d43addc76ef05372ba0847eaed6a
Amin Saffarnejad <vira1368@gmail.com>
2024-11-12 22:02:13 +0330

    Added RoleConfiguration.

---
+++ DashboardBlazor/Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder
+                .ToTable("Roles")
+                .HasData(new Role { Id = 1, Name = "Admin", NormalizedName = "ADMIN" });
+        }
+    }
+}
