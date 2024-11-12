3a0a2d55895025f551a65d9a4c06a23e59f61eaa
Amin Saffarnejad <vira1368@gmail.com>
2024-11-13 02:24:43 +0330

    Added UserTokenConfiguration.

---
+++ DashboardBlazor/Data/Configurations/UserTokenConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class UserTokenConfiguration : IEntityTypeConfiguration<UserToken>
+    {
+        public void Configure(EntityTypeBuilder<UserToken> builder)
+        {
+            builder.ToTable("UserTokens");
+        }
+    }
+}
