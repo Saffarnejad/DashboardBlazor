57dfe31b923b540d962e43b848c1d2bcc797f35b
Amin Saffarnejad <vira1368@gmail.com>
2024-11-14 23:16:16 +0330

    Configurations OnModelCreating.

--- DashboardBlazor/Data/ApplicationDbContext.cs
+++ DashboardBlazor/Data/ApplicationDbContext.cs
@@ -1,10 +1,23 @@
+using DashboardBlazor.Data.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DashboardBlazor.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User, Role, int>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
         IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            new RoleConfiguration().Configure(builder.Entity<Role>());
+            new RoleClaimConfiguration().Configure(builder.Entity<RoleClaim>());
+            new UserConfiguration().Configure(builder.Entity<User>());
+            new UserClaimConfiguration().Configure(builder.Entity<UserClaim>());
+            new UserLoginConfiguration().Configure(builder.Entity<UserLogin>());
+            new UserRoleConfiguration().Configure(builder.Entity<UserRole>());
+            new UserTokenConfiguration().Configure(builder.Entity<UserToken>());
+        }
     }
 }
