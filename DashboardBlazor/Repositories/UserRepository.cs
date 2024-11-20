cf958e506ae9f55546d1b5cb16185a3fc7a53fd8
Amin Saffarnejad <vira1368@gmail.com>
2024-11-20 23:25:51 +0330

    Implemented UserRepository.

---
+++ DashboardBlazor/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using DashboardBlazor.Data;
+using DashboardBlazor.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DashboardBlazor.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetAsync(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => id == u.Id);
+            if (user is null)
+            {
+                return new User();
+            }
+            return user;
+        }
+    }
+}
