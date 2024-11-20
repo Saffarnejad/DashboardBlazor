f6a6499884d1013ca20cc1e34ce3da4b47c4af0c
Amin Saffarnejad <vira1368@gmail.com>
2024-11-20 23:24:20 +0330

    Added UserRepository interface.

---
+++ DashboardBlazor/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using DashboardBlazor.Data;
+
+namespace DashboardBlazor.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<IEnumerable<User>> GetAllAsync();
+        public Task<User> GetAsync(int id);
+    }
+}
