926fdabafdaa838654de15f6ae59ce75f5aa18db
Amin Saffarnejad <vira1368@gmail.com>
2024-11-28 23:16:18 +0330

    Added ICountryRepository.

---
+++ DashboardBlazor/Repositories/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DashboardBlazor.Data;
+
+namespace DashboardBlazor.Repositories.Interfaces
+{
+    public interface ICountryRepository
+    {
+        public Task<Country> CreateAsync(Country country);
+        public Task<bool> DeleteAsync(int id);
+        public Task<IEnumerable<Country>> GetAllAsync();
+        public Task<Country> GetAsync(int id);
+        public Task<Country> UpdateAsync(Country country);
+    }
+}
