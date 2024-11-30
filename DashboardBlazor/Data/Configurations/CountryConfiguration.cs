199cfa7f8e0eea1fe936ba18fea792e78be58509
Amin Saffarnejad <vira1368@gmail.com>
2024-11-30 23:06:29 +0330

    Added CountryConfiguration.

---
+++ DashboardBlazor/Data/Configurations/CountryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasData(new Country { Id = 1, Name = "Iran" });
+        }
+    }
+}
