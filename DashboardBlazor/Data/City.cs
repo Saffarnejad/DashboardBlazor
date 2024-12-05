2ca7ddb255c4b9b34fb005e8b714f3213b7d564e
Amin Saffarnejad <vira1368@gmail.com>
2024-12-05 21:56:15 +0330

    Added City model

---
+++ DashboardBlazor/Data/City.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DashboardBlazor.Data
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int ProvinceId { get; set; }
+        public Province Province { get; set; }
+    }
+}
