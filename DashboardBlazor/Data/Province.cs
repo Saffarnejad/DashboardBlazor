0f2560b3dd9d7f35a43f05900979205138d8c36b
Amin Saffarnejad <vira1368@gmail.com>
2024-12-05 21:53:36 +0330

    Added Province model.

---
+++ DashboardBlazor/Data/Province.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DashboardBlazor.Data
+{
+    public class Province
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
