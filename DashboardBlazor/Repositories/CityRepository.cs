b1581b7d1c3187f6b6272ae38c2ea0733e47bd02
Amin Saffarnejad <vira1368@gmail.com>
2024-12-06 16:05:43 +0330

    Implemented CityRepository.

---
+++ DashboardBlazor/Repositories/CityRepository.cs
@@ -0,0 +1,63 @@
+﻿using DashboardBlazor.Data;
+using DashboardBlazor.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DashboardBlazor.Repositories
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CityRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<City> CreateAsync(City city)
+        {
+            await _context.Cities.AddAsync(city);
+            await _context.SaveChangesAsync();
+            return city;
+
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var city = await _context.Cities.FirstOrDefaultAsync(c => c.Id == id);
+            if (city != null)
+            {
+                _context.Cities.Remove(city);
+                return (await _context.SaveChangesAsync()) > 0;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<City>> GetAllAsync()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+
+        public async Task<City> GetAsync(int id)
+        {
+            var city = await _context.Cities.FirstOrDefaultAsync(c => c.Id == id);
+            if (city != null)
+            {
+                return city;
+            }
+            return new City();
+        }
+
+        public async Task<City> UpdateAsync(City city)
+        {
+            var cityInDb = await _context.Cities.FirstOrDefaultAsync(c => c.Id == city.Id);
+            if (cityInDb != null)
+            {
+                cityInDb.Name = city.Name;
+                _context.Cities.Update(cityInDb);
+                await _context.SaveChangesAsync();
+                return city;
+            }
+            return new City();
+        }
+    }
+}
