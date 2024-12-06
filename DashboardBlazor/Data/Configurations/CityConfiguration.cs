efc1f3279737fdca94730659fbb2bae03a3014d8
Amin Saffarnejad <vira1368@gmail.com>
2024-12-06 15:45:27 +0330

    Added CityConfiguration.

---
+++ DashboardBlazor/Data/Configurations/CityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasData(new City { Id = 1, Name = "Tehran", ProvinceId = 1 });
+        }
+    }
+}
