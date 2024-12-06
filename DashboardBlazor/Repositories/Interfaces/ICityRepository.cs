16186406012707f7579adb4cc2855fa1a2122f3b
Amin Saffarnejad <vira1368@gmail.com>
2024-12-06 16:05:15 +0330

    Added ICityRepository.

---
+++ DashboardBlazor/Repositories/Interfaces/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using DashboardBlazor.Data;
+
+namespace DashboardBlazor.Repositories.Interfaces
+{
+    public interface ICityRepository
+    {
+        public Task<City> CreateAsync(City city);
+        public Task<bool> DeleteAsync(int id);
+        public Task<IEnumerable<City>> GetAllAsync();
+        public Task<City> GetAsync(int id);
+        public Task<City> UpdateAsync(City city);
+    }
+}
