8582484de0615a2f8d7c3e8b26f84ada5c111266
Amin Saffarnejad <vira1368@gmail.com>
2024-12-06 16:03:38 +0330

    Added IProvinceRepository.

---
+++ DashboardBlazor/Repositories/Interfaces/IProvinceRepository.cs
@@ -0,0 +1,13 @@
+﻿using DashboardBlazor.Data;
+
+namespace DashboardBlazor.Repositories.Interfaces
+{
+    public interface IProvinceRepository
+    {
+        public Task<Province> CreateAsync(Province province);
+        public Task<bool> DeleteAsync(int id);
+        public Task<IEnumerable<Province>> GetAllAsync();
+        public Task<Province> GetAsync(int id);
+        public Task<Province> UpdateAsync(Province province);
+    }
+}
