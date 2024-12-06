4df5f21e3f92290346fd94d187df94a24125382f
Amin Saffarnejad <vira1368@gmail.com>
2024-12-06 15:44:23 +0330

    Added ProvinceConfiguration.

---
+++ DashboardBlazor/Data/Configurations/ProvinceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class ProvinceConfiguration : IEntityTypeConfiguration<Province>
+    {
+        public void Configure(EntityTypeBuilder<Province> builder)
+        {
+            builder.HasData(new Province { Id = 1, Name = "Tehran", CountryId = 1 });
+        }
+    }
+}
