18f385c2358d58087ec027a5dadbe377bdc52dc1
Amin Saffarnejad <vira1368@gmail.com>
2024-12-09 03:11:54 +0330

    Added confirmation script.

---
+++ DashboardBlazor/wwwroot/js/confirmation.js
@@ -0,0 +1,16 @@
+﻿window.ShowSwal = function (title, text, icon) {
+    return new Promise(resolve => {
+        Swal.fire({
+            title,
+            text,
+            icon,
+            showCancelButton: true,
+            //confirmButtonColor: "#3085d6",
+            //cancelButtonColor: "#d33",
+            cancelButtonText: "خیر",
+            confirmButtonText: "بله"
+        }).then((result) => {
+            resolve(result.isConfirmed);
+        });
+    });
+}
