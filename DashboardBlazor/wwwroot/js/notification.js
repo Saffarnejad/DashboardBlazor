86ecaa34bc04a9ca3e1b75bc637fdfb08ae499ae
Amin Saffarnejad <vira1368@gmail.com>
2024-12-09 03:12:13 +0330

    Added notification script.

---
+++ DashboardBlazor/wwwroot/js/notification.js
@@ -0,0 +1,34 @@
+﻿window.ShowToastr = function (type, message) {
+    //var options = {
+    //    "closeButton": true,
+    //    "debug": false,
+    //    "newestOnTop": false,
+    //    "progressBar": true,
+    //    "positionClass": "toast-bottom-center",
+    //    "preventDuplicates": false,
+    //    "showDuration": "300",
+    //    "hideDuration": "1000",
+    //    "timeOut": "5000",
+    //    "extendedTimeOut": "1000",
+    //    "showEasing": "swing",
+    //    "hideEasing": "linear",
+    //    "showMethod": "fadeIn",
+    //    "hideMethod": "fadeOut"
+    //}
+
+    //toastr.options.rtl = true;
+    toastr.options.positionClass = "toast-top-left";
+
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "warning") {
+        toastr.warning(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+    if (type == "info") {
+        toastr.info(message);
+    }
+}
