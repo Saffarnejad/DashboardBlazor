6c416de940723bdbcb356c450a97925f25715b72
Amin Saffarnejad <vira1368@gmail.com>
2024-12-10 23:59:17 +0330

    Added ShowToast script.

--- DashboardBlazor/wwwroot/js/confirmation.js
+++ DashboardBlazor/wwwroot/js/confirmation.js
@@ -4,13 +4,36 @@
             title,
             text,
             icon,
-            showCancelButton: true,
-            //confirmButtonColor: "#3085d6",
-            //cancelButtonColor: "#d33",
+            iconHtml: "؟",
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#6e7881",
+            confirmButtonText: "بله",
             cancelButtonText: "خیر",
-            confirmButtonText: "بله"
+            showCancelButton: true
         }).then((result) => {
             resolve(result.isConfirmed);
         });
+    });
+}
+
+window.ShowToast = function (title, icon) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-start",
+        iconColor: 'white',
+        customClass: {
+            popup: 'colored-toast',
+        },
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon,
+        title
     });
 }
