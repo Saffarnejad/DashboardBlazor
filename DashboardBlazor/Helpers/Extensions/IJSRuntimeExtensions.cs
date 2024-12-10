81b863eda892a7a7ff2af548e25631a4dd5cbd41
Amin Saffarnejad <vira1368@gmail.com>
2024-12-11 00:07:34 +0330

    Added IJSRuntimeExtensions.

---
+++ DashboardBlazor/Helpers/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop;
+
+namespace DashboardBlazor.Helpers.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task Toastr(this IJSRuntime js, ToastrType type, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", type.ToString().ToLower(), message);
+        }
+
+        public static async Task<bool> Confirmation(this IJSRuntime js, string title, string text, SwalIcon icon)
+        {
+            return await js.InvokeAsync<bool>("ShowSwal", title, text, icon.ToString().ToLower());
+        }
+
+        public static async Task Toast(this IJSRuntime js, string title, SwalIcon icon)
+        {
+            await js.InvokeVoidAsync("ShowToast", title, icon.ToString().ToLower());
+        }
+    }
+
+    public enum ToastrType
+    {
+        Success = 0,
+        Warning = 1,
+        Error = 2,
+        Info = 3
+    }
+
+    public enum SwalIcon
+    {
+        Success = 0,
+        Warning = 1,
+        Error = 2,
+        Info = 3,
+        Question = 4
+    }
+}
