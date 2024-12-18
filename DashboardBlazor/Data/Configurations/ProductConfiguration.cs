5bdafcacd750262fd62dea709f64a314ffaea12b
Amin Saffarnejad <vira1368@gmail.com>
2024-12-18 23:41:42 +0330

    Added ProductConfiguration.

---
+++ DashboardBlazor/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DashboardBlazor.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product { Id = 1, Name = "Keyboard", CreatedDateTime = new DateTime(2024, 12, 10) });
+            builder.HasData(new Product { Id = 2, Name = "Mouse", CreatedDateTime = new DateTime(2024, 12, 10) });
+            builder.HasData(new Product { Id = 3, Name = "Monitor", CreatedDateTime = new DateTime(2024, 12, 11) });
+            builder.HasData(new Product { Id = 4, Name = "Modem", CreatedDateTime = new DateTime(2024, 12, 12) });
+        }
+    }
+}
