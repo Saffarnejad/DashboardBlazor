82e64809e0ed911cddb0fef087b56caa458abd71
Amin Saffarnejad <vira1368@gmail.com>
2024-12-18 23:40:27 +0330

    Added Prodact model.

---
+++ DashboardBlazor/Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DashboardBlazor.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
