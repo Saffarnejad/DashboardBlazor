e64f640586adceaf8921e4d4155c97f9cf0a6762
Amin Saffarnejad <vira1368@gmail.com>
2024-12-19 13:49:21 +0330

    Added IProductRepository.

---
+++ DashboardBlazor/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using DashboardBlazor.Data;
+
+namespace DashboardBlazor.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<Product> CreateAsync(Product product);
+        public Task<bool> DeleteAsync(int id);
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> GetAsync(int id);
+        public Task<Product> UpdateAsync(Product product);
+    }
+}
