519c61022d9766cf6e4f9b3a99e35070f595d105
Amin Saffarnejad <vira1368@gmail.com>
2024-12-28 20:05:12 +0330

    Defined PersianDatepickerInit function.

---
+++ DashboardBlazor/wwwroot/js/BlazorPersianDatepicker.js
@@ -0,0 +1,11 @@
+﻿window.PersianDatepickerInit = function (startDate, endDate) {
+    $(".bpdp").persianDatepicker({
+        persianNumbers: true,
+        formatDate: "YYYY/0M/0D",
+        startDate,
+        endDate,
+        cellWidth: 40,
+        cellHeight: 30,
+        fontSize: 14
+    });
+}
